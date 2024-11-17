2fc970d0f76330b76449130256de5da3a333e3dd
darkard2003 <kaushikchowdhury231@gmail.com>
2024-11-18 01:45:28 +0530

    Added check

--- archstrap.sh
+++ archstrap.sh
@@ -1,12 +1,22 @@
 # Ask for sudo password upfront
 sudo -v
 
+set -e
+
+function install_package {
+    echo "Installing $1"
+    if ! [ command -v $1 &> /dev/null ]; then
+        sudo pacman -S $1 --noconfirm
+        echo "Installed $1"
+    else
+        echo "$1 is already installed"
+    fi
+    sleep 1
+}
+
 # Install packages in swayreq file
 for package in $(cat swayreq); do
-    echo "Installing $package"
-    sudo pacman -S $package --noconfirm
-    echo "Installed $package"
-    sleep 1
+    install_package $package
 done
 
 # Move sway config to config folder
