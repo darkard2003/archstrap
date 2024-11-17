38e61c15c3db65a05365c31de7747f3ff44ce5cc
darkard2003 <kaushikchowdhury231@gmail.com>
2024-11-18 01:10:20 +0530

    Added nvim and kitty config

--- archstrap.sh
+++ archstrap.sh
@@ -14,3 +14,15 @@ git clone https://github.com/darkard2003/swayconfig.git ~/.config/sway
 
 # Move waybar config to config folder
 git clone https://github.com/darkard2003/waybarconfig.git ~/.config/waybar
+
+if ! [ command -v nvim &> /dev/null ]; then
+    echo "Installing nvim"
+    sudo pacman -S neovim --noconfirm
+    echo "Installed nvim"
+fi
+
+# Move nvim config to config folder
+git clone https://github.com/darkard2003/nvim_config_darkchoclate.git ~/.config/nvim
+
+# Move kitty config to config folder
+git clone https://github.com/darkard2003/kittyconfig.git ~/.config/kitty
